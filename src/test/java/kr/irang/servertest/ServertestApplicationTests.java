7158544de0eca4018b56e3291d35c3817f95ab08
Irang <yupk1928@naver.com>
2022-07-04 13:02:23 +0900

    first commit

---
+++ src/test/java/kr/irang/servertest/ServertestApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.irang.servertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServertestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
